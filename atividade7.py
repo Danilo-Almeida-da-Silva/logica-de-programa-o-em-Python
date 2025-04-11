13fcce1a4ba627f98191b3f099d269a0cb53200b
Danilo Almeida <danilo.laet52@gmail.com>
2025-04-10 21:35:34 -0300

    feat: leitor de numero booleano

---
+++ atividade7.py
@@ -0,0 +1,10 @@
+#Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos são VERDADEIROS ou FALSOS.
+
+valor1 = bool(input('digite qualquer valor booleanos : ' ))
+valor2 = bool(input('digite qualquer valor booleanos :'))
+
+if valor1 != valor2:
+  print('os dois valores são verdadeiros')
+else:
+  valor1 == valor2
+  print('os dois valores são falsos')
