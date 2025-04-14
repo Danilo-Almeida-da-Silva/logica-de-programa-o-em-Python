fe5d4a39481046a62aff905b8c23d7a686a4f144
Danilo Almeida <danilo.laet52@gmail.com>
2025-04-14 20:01:44 -0300

    Feat: atividade logica de  dicionario

---
+++ atividade14_dicionario.py
@@ -0,0 +1,24 @@
+#DICIONARIO
+
+## Os dicionarios representam coleções de dados que contém na sua estrutura.
+
+pessoas = ['danilo', 'ana', 'joao'] #Listas
+
+
+pessoas = {'pedro': 18, 'luana' : 20} #Dicionarios
+
+
+print(pessoas['pedro'])
+
+print(pessoas['luana'])
+
+
+pessoas.keys() #mostra as chaves
+
+pessoas.values() #mostra valores
+
+
+pessoas.items() # mostra todo dicionário
+
+pessoas.pop #revome e mostra o ultimo item removido
+
